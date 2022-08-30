1d7ed403631c134cb7f887b861edaa248754b462
hisumin46 <rtn2001@naver.com>
2022-08-30 17:02:45 +0900

    프로젝트 생성 및 빌드
    - 스프링 환경 세팅

---
+++ mine/src/main/java/com/sumin/mine/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.sumin.mine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+  @GetMapping("/index")
+  public String index() {
+    return "hello";
+  } 
+}
